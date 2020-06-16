0e8d35eddb3c179c778a475ab701416f5330fabd
Bilel-Gharbi <hb.bilel@gmail.com>
2020-06-16 19:00:26 +0100

    depp

--- server.js
+++ server.js
@@ -6,17 +6,17 @@ const { PORT } = require("./config");
 //init express server
 const app = express();
 
-// for deployment
-if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static("./client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
-
 //we load all app middleware exp : "cors , morgan , json ...."
 loader(app);
+
+if (process.env.NODE_ENV === "production") {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, "client/build")));
+  // Handle React routing, return all requests to React app
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
 
 //run the server
 app.listen(PORT, () => console.log(`App listening on port ${PORT} !`));
