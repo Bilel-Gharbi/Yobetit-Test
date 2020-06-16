2ce55f77bc2dd09dad62a9239a2255222000f555
Bilel-Gharbi <hb.bilel@gmail.com>
2020-06-16 18:41:02 +0100

    dep 4

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("./client/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve("client", "build", "index.html"));
+    __dirname = "/";
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
